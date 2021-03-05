2f5e6e490972f80d8d112c403bb9784a12a62454
aistidedevnholle <solitarynothingtoprovetoanyone@gmail.com>
2021-03-05 22:29:37 +0000

    okm

--- lib/pages/client/clienthomepage.dart
+++ lib/pages/client/clienthomepage.dart
@@ -40,137 +40,140 @@ class _ClientHomePageState extends State<ClientHomePage> with TickerProviderStat
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        title: Text('Service Gz'),
-        actions: [
-          StreamBuilder(
-              stream: FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser.uid)
-                  .collection("reservation")
-                  .snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('');
-                } else if (!snapshot.hasData) {
-                  return Text('');
-                }else if (snapshot?.data?.docs?.length == 0) {
-                  return Text('');
-                }
-                return Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: IconButton(
-                    onPressed: (){
-                      Navigator.push(
-                          context, PageTransition(
-                          child: OrderTrackPage(),
-                          type: PageTransitionType.fadeIn,
-                          duration: Duration(milliseconds: 500)
-                      ));
-                    },
-                    tooltip: 'notification',
-                    icon: Stack(
-                      alignment:Alignment.topRight,
-                      children: [
-                        Icon(Icons.notifications,size: 37,color: Colors.white),
-                        Container(
-                          height: 25.0,
-                          width: 20.0,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(16.0)
-                          ),
-                          child: Center(
-                              child: Text('${snapshot?.data?.docs?.length}',style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold,fontSize: 17))),
-                        )
-                      ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child:Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text('Service Gz'),
+          actions: [
+            StreamBuilder(
+                stream: FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser.uid)
+                    .collection("reservation")
+                    .snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('');
+                  } else if (!snapshot.hasData) {
+                    return Text('');
+                  }else if (snapshot?.data?.docs?.length == 0) {
+                    return Text('');
+                  }
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: IconButton(
+                      onPressed: (){
+                        Navigator.push(
+                            context, PageTransition(
+                            child: OrderTrackPage(),
+                            type: PageTransitionType.fadeIn,
+                            duration: Duration(milliseconds: 500)
+                        ));
+                      },
+                      tooltip: 'notification',
+                      icon: Stack(
+                        alignment:Alignment.topRight,
+                        children: [
+                          Icon(Icons.notifications,size: 37,color: Colors.white),
+                          Container(
+                            height: 25.0,
+                            width: 20.0,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(16.0)
+                            ),
+                            child: Center(
+                                child: Text('${snapshot?.data?.docs?.length}',style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold,fontSize: 17))),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }
-          )
-        ],
-      ),
-      drawer: Drawer(),
-      body:  Container(
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            Container(
-              height: 30,
-              child: Center(
-                child: Marquee(
-                  text: "un service à votre écoute 24h/24 7j/7",
-                  style: TextStyle(color: Colors.black, fontSize: 17.0),
-                  scrollAxis: Axis.horizontal,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  blankSpace: 5.0,
-                  velocity: 100.0,
-                  pauseAfterRound: Duration(seconds: 1),
-                  startPadding: 1.0,
-                  accelerationDuration: Duration(seconds: 2),
-                  accelerationCurve: Curves.linear,
-                  decelerationDuration: Duration(milliseconds: 900),
-                  decelerationCurve: Curves.easeOut,
-                ),
-              ),
-            ),
-
-            Expanded(
-              child: gzsList.isEmpty ?
-              Center(
-                child: Text('Chargement en cours...'),
-              )
-                  :
-              SingleChildScrollView(
-                child: Column(
-                  children:  buildGzChoice(),
-                ),
-              ),
-            ),
+                  );
+                }
+            )
           ],
         ),
-      ),
+        drawer: Drawer(),
+        body:  Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Container(
+                height: 30,
+                child: Center(
+                  child: Marquee(
+                    text: "un service à votre écoute 24h/24 7j/7",
+                    style: TextStyle(color: Colors.black, fontSize: 17.0),
+                    scrollAxis: Axis.horizontal,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    blankSpace: 5.0,
+                    velocity: 100.0,
+                    pauseAfterRound: Duration(seconds: 1),
+                    startPadding: 1.0,
+                    accelerationDuration: Duration(seconds: 2),
+                    accelerationCurve: Curves.linear,
+                    decelerationDuration: Duration(milliseconds: 900),
+                    decelerationCurve: Curves.easeOut,
+                  ),
+                ),
+              ),
 
-       //AVEC LA CASE A COCHER buildGzChoice FAIT DE CETTE MANIERE LA MISE A JOUR
-      //EST FAITE POUR LA CONDITION QUE JE FAIS POUR AFFICHER OU PAS LE bottomNavigationBar
-       bottomNavigationBar:gzChosen.keys.isEmpty ?
-       Container(
-         height: 45,
-         child: Text(''),
-       )
-           : Container(
-         width: MediaQuery.of(context).size.width,
-         color: Colors.teal,
-         height: 45,
-         child: FlatButton(
-             onPressed: () {
-               print('$gzChosen $gzsList');
-               Navigator.push(context, PageTransition(
-                   child: ValidateOrderPage(
-                     userName: widget.userName,
-                     userCommune: widget.commune,
-                     userCity: widget.city,
-                     userFirstNumber: widget.phoneNumber,
-                     userQuarter: widget.quarter,
-                     userSecondNumber: widget.secondNumber,
-                     gzsChosen: gzsChosen,
-                   ),
-                   type: PageTransitionType.slideInLeft,
-                   duration: Duration(milliseconds: 500)));
+              Expanded(
+                child: gzsList.isEmpty ?
+                Center(
+                  child: Text('Chargement en cours...'),
+                )
+                    :
+                SingleChildScrollView(
+                  child: Column(
+                    children:  buildGzChoice(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        //AVEC LA CASE A COCHER buildGzChoice FAIT DE CETTE MANIERE LA MISE A JOUR
+        //EST FAITE POUR LA CONDITION QUE JE FAIS POUR AFFICHER OU PAS LE bottomNavigationBar
+        bottomNavigationBar:gzChosen.keys.isEmpty ?
+        Container(
+          height: 45,
+          child: Text(''),
+        )
+            : Container(
+          width: MediaQuery.of(context).size.width,
+          color: Colors.teal,
+          height: 45,
+          child: FlatButton(
+              onPressed: () {
+                print('$gzChosen $gzsList');
+                Navigator.push(context, PageTransition(
+                    child: ValidateOrderPage(
+                      userName: widget.userName,
+                      userCommune: widget.commune,
+                      userCity: widget.city,
+                      userFirstNumber: widget.phoneNumber,
+                      userQuarter: widget.quarter,
+                      userSecondNumber: widget.secondNumber,
+                      gzsChosen: gzsChosen,
+                    ),
+                    type: PageTransitionType.slideInLeft,
+                    duration: Duration(milliseconds: 500)));
 
 
-               for(int i = 0; i < gzsList.keys.length; i++){
-                 if(gzsList[gzsList.keys.toList()[i]][0] == true){
-                   setState(() {
-                     gzsList[gzsList.keys.toList()[i]][0] = false;
-                     gzChosen.clear();
-                   });
-                 }
-               }
-             },
-             child: Text('Suivant')),
-       ),
+                for(int i = 0; i < gzsList.keys.length; i++){
+                  if(gzsList[gzsList.keys.toList()[i]][0] == true){
+                    setState(() {
+                      gzsList[gzsList.keys.toList()[i]][0] = false;
+                      gzChosen.clear();
+                    });
+                  }
+                }
+              },
+              child: Text('Suivant')),
+        ),
+      )
     );
   }
 
