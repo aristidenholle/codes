3b5826311663a53345378b76d12ce89f8d02084c
aristidenholle <melref117@gmail.com>
2021-03-05 23:05:40 +0000

    somme changes

--- lib/pages/client/clienthomepage.dart
+++ lib/pages/client/clienthomepage.dart
@@ -33,14 +33,6 @@ class _ClientHomePageState extends State<ClientHomePage> with TickerProviderStat
 
   @override
   void initState() {
-    FirebaseFirestore.instance.collection('Gzlist')
-    .get().then((value){
-      for(QueryDocumentSnapshot gz in value.docs){
-        setState(() {
-          gzsList.addAll({"${gz.data()['gzname']}": [false, gz.data()['gzprice'], gz.data()['gzimage']]});
-        });
-      }
-    }).catchError((er) => print('UNABLE TO GET GZ LIST $er'));
     super.initState();
   }
 
@@ -234,7 +226,86 @@ class _ClientHomePageState extends State<ClientHomePage> with TickerProviderStat
 //TO AVOID MANY READ IN FIREBASE
 
   void setData({Map<String, List> gzList}) async{
-    SharedPreferences preferences = await SharedPreferences.getInstance();
+    SharedPreferences set = await SharedPreferences.getInstance();
+
+    if(gzsList.keys.isNotEmpty){
+      set.setString(keyToCheckIFValueItSaved, gzsList.keys.first);
+      set.setInt(gzNumber, gzsList.keys.length);
+    }
+
+    //({"${gz.data()['gzname']}": [false, gz.data()['gzprice'], gz.data()['gzimage']]});
+    for(var i = 0; i < gzsList.keys.length; i++){
+      set.setString("keyName$i", gzsList.keys.toList()[i]);
+    }
+    for(var i = 0; i < gzsList.keys.length; i++){
+      set.setString("keyBool$i", gzsList[gzsList.keys.toList()[i]][0]);
+    }
+    for(var i = 0; i < gzsList.keys.length; i++){
+      set.setString("keyPrice$i", gzsList[gzsList.keys.toList()[i]][1]);
+    }
+
+    for(var i = 0; i < gzsList.keys.length; i++){
+      set.setString("keyImg$i", gzsList[gzsList.keys.toList()[i]][2]);
+    }
+
+    getData();
+  }
+
+  getData() async{
+    SharedPreferences get = await SharedPreferences.getInstance();
+    var getFirstKeyName = get.getString(keyToCheckIFValueItSaved);
+    var getGzNumber = get.getString(gzNumber);
+    print('DATA $getFirstKeyName, $getGzNumber');
+
+    for(var i = 0; i < gzsList.keys.length; i++){
+      var getGzName = get.getString("keyName$i");
+      if(getGzName != null){
+        print('DATA :$getGzName');
+      }
+    }
+    for(var i = 0; i < gzsList.keys.length; i++){
+      var getGzBool = get.getString("keyBool$i");
+      if(getGzBool != null){
+        print('DATA :$getGzBool');
+      }
+    }
+    for(var i = 0; i < gzsList.keys.length; i++){
+      var getGzPrice = get.getString("keyPrice$i");
+      if(getGzPrice != null){
+        print('DATA :$getGzPrice');
+      }
+    }
+
+    for(var i = 0; i < gzsList.keys.length; i++){
+      var getGzImg = get.getString("keyImg$i");
+      if(getGzImg != null){
+        print('DATA :$getGzImg');
+      }
+    }
+
+  }
+
+  void checkIfFirstGzNameSaved() async{
+    SharedPreferences get = await SharedPreferences.getInstance();
+    var getFirstKeyName = get.getString(keyToCheckIFValueItSaved);
+
+    if(getFirstKeyName != null){
+      print('DATA IS SAVED IN PERSISTENT STORAGE $getFirstKeyName');
+
+    }else{
+      print('DATA NOT SAVED YET, MAKE FIRST READ');
+      FirebaseFirestore.instance.collection('Gzlist')
+          .get().then((value){
+        for(QueryDocumentSnapshot gz in value.docs){
+          setState(() {
+            gzsList.addAll({"${gz.data()['gzname']}": [false, gz.data()['gzprice'], gz.data()['gzimage']]});
+          });
+        }
+
+        setData(gzList: gzsList);
+        getData();
+      }).catchError((er) => print('UNABLE TO GET GZ LIST $er'));
+    }
   }
 
 }
